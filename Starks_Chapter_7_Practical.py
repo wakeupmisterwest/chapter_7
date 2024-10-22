b8bdffcf4cd8cde9eb6c322da114acb9026fbcfe
Julian <julian-jordan.starks@fwcsstudents.org>
2024-10-22 11:47:36 -0400

    Add files via upload

--- Starks_Chapter_7_Practical.py
+++ Starks_Chapter_7_Practical.py
@@ -6,24 +6,25 @@ input_count = 0
 while input_count < max_input:
     name = input("\nWhat is your name? ")
 
-    if name not in fast_food_list:
+    if name not in fast_food:
         fast_food[name] = []
 
     while True:
         response = input("What is your favorite fast food place? ")
-        fast_food[name] = response.append(fast_food)
 
-        if response.strip().lower() not in (fast_food.lower() for fast_food in
+        if response.strip().lower() not in (place.lower() for place in
                                             fast_food_list):
-            fast_food_list.append(response)
+            fast_food_list.append(response.strip())
+
+        fast_food[name].append(response.strip())
 
         repeat = input(
             "Would you like to add another place? (yes)/(no): ").strip().lower()
         if repeat != 'yes':
             break
 
-input_count += 1
+    input_count += 1
 
 print("\n--- Results ---")
-for name, fast_food in fast_food.items():
-    print(f"{name}'s favorite fast food places are {fast_food}")
+for name, places in fast_food.items():
+    print(f"{name}'s favorite fast food places are {', '.join(places)}")
